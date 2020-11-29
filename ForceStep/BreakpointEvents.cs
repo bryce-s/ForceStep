d8c210009c7f12d4d27d2b88849ba10be8ba03ef
bryce-s <bryces@umich.edu>
2020-11-29 17:52:37 -0500

    working on events

---
+++ ForceStep/BreakpointEvents.cs
@@ -0,0 +1,43 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForceStepConstants;
+
+namespace ForceStep
+{
+    class BreakpointEvents
+    {
+
+        EnvDTE.DTE m_Dte;
+        
+        BreakpointEvents(EnvDTE.DTE dte)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            m_Dte = dte;
+            var debuggerEvents = dte.Events.DebuggerEvents;
+            debuggerEvents.OnEnterBreakMode += OnEnterBreakMode;
+        }
+
+        private void OnEnterBreakMode(dbgEventReason reason, ref dbgExecutionAction executionAction)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            if (reason == dbgEventReason.dbgEventReasonStep)
+            {
+               foreach (Breakpoint bp in m_Dte.Debugger.Breakpoints)
+               {
+                    if (bp.Tag == BreakpointStatusCodes.SuspendedFromStep)
+                    {
+                        bp.Enabled = true;
+                        bp.Tag = "";
+                    }
+               }
+            }
+
+
+        }
+
+    }
+}
---
+++ ForceStep/ForceStepConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForceStepConstants
+{
+        public static class BreakpointStatusCodes
+        {
+            public static string Suspended = "[s]";
+            public static string SuspendedFromStep = "[step]";
+            public static string Active = "";
+        }
+
+}
+
