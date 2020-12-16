e4f0f88bb52e30ec0d73475c9f825bc5136c5e76
bryce-s <bryces@umich.edu>
2020-12-15 21:04:29 -0500

    recreated package for new commands

---
+++ ForceStep/BreakpointManager.cs
@@ -0,0 +1,111 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForceStepConstants;
+
+namespace ForceStep
+{
+    class BreakpointManager
+    {
+
+        private readonly AsyncPackage m_package;
+        public BreakpointManager(AsyncPackage package)
+        {
+            m_package = package;
+        }
+
+        /// <summary>
+        /// clears and re-enables all breakpoints set by set or continue operations.
+        /// need to run on mode change.
+        /// </summary>
+        public void DisableSuspendedFromOperationBreakpoints()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dte = UtilityMethods.GetDTE(m_package);
+
+            foreach (Breakpoint bp in dte.Debugger.Breakpoints)
+            {
+                if (BreakpointStatusCodes.SuspendedFromOperation().Contains(bp.Tag))
+                {
+                    bp.Enabled = true;
+                    bp.Tag = "";
+                }
+            }
+        }
+
+        private void SuspendBreakpoints(string statusCode)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE dte = UtilityMethods.GetDTE(m_package);
+
+            foreach (EnvDTE.Breakpoint bp in dte.Debugger.Breakpoints)
+            {
+                if (bp.Enabled)
+                {
+                    bp.Tag = statusCode;
+                    bp.Enabled = false;
+                }
+            }
+        }
+
+        private void RestoreBreakpointsFromSuspension(string type)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE dte = UtilityMethods.GetDTE(m_package);
+
+            foreach (EnvDTE.Breakpoint bp in dte.Debugger.Breakpoints)
+            {
+                if (bp.Tag == type)
+                {
+                    bp.Tag = BreakpointStatusCodes.Active;
+                    bp.Enabled = true;
+                }
+            }
+        }
+
+
+        public void SaveAndSuspendActiveBreakpoints(SaveBreakpointReason reason)
+        {
+            if (reason == SaveBreakpointReason.ForceStep)
+            {
+                SuspendBreakpoints(BreakpointStatusCodes.SuspendedFromStep);
+            }
+            if (reason == SaveBreakpointReason.ForceStepOut)
+            {
+                SuspendBreakpoints(BreakpointStatusCodes.SuspendedFromStepOut);
+            }
+            if (reason == SaveBreakpointReason.ForceContinue)
+            {
+                SuspendBreakpoints(BreakpointStatusCodes.SuspendedFromContinue);
+            }
+            if (reason == SaveBreakpointReason.Manual)
+            {
+                SuspendBreakpoints(BreakpointStatusCodes.SuspendedManually);
+            }
+        }
+
+        public void RestoreSavedBreakpoints(SaveBreakpointReason reason)
+        {
+            if (reason == SaveBreakpointReason.Manual)
+            {
+                RestoreBreakpointsFromSuspension(BreakpointStatusCodes.SuspendedManually);
+            }
+            if (reason == SaveBreakpointReason.ForceStep)
+            {
+                RestoreBreakpointsFromSuspension(BreakpointStatusCodes.SuspendedFromStep);
+            }
+            if (reason == SaveBreakpointReason.ForceStepOut)
+            {
+                RestoreBreakpointsFromSuspension(BreakpointStatusCodes.SuspendedFromStepOut);
+            }
+        }
+
+    }
+}
---
+++ ForceStep/ForceStepConstants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForceStepConstants
+{
+    public static class BreakpointStatusCodes
+    {
+        public static string SuspendedManually = "[manual]";
+        public static string SuspendedFromStep = "[step]";
+        public static string SuspendedFromStepOut = "[sout]";
+        public static string SuspendedFromContinue = "[continue]";
+        public static string Active = "";
+
+        private static List<string> StepOperations = null;
+
+        public static List<string> SuspendedFromOperation()
+        {
+            if (StepOperations == null)
+            {
+                StepOperations = new List<string>();
+                StepOperations.Add(SuspendedFromStep);
+                StepOperations.Add(SuspendedFromStepOut);
+                StepOperations.Add(SuspendedFromContinue);
+            }
+            return StepOperations;
+        }
+    }
+
+    public enum SaveBreakpointReason
+    {
+        Manual = 0,
+        ForceStep = 1,
+        ForceStepOut = 2,
+        ForceContinue = 3,
+    }
+
+}
