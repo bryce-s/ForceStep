5042d66a1a8d2614c66a1f5c173b1ce963e8c34d
bryce-s <bryces@umich.edu>
2020-12-21 20:41:22 -0500

    remove comments

--- ForceStep/ManuallyEnable.cs
+++ ForceStep/ManuallyEnable.cs
@@ -9,32 +9,14 @@ using Task = System.Threading.Tasks.Task;
 
 namespace ForceStep
 {
-    /// <summary>
-    /// Command handler
-    /// </summary>
     internal sealed class ManuallyEnable
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
         public const int CommandId = 4133;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
         public static readonly Guid CommandSet = new Guid("5499ab04-087c-4366-bd73-ff583b883993");
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
         private readonly AsyncPackage package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ManuallyEnable"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
         private ManuallyEnable(AsyncPackage package, OleMenuCommandService commandService)
         {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
@@ -48,18 +30,12 @@ namespace ForceStep
 
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
         public static ManuallyEnable Instance
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
         private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
         {
             get
@@ -68,27 +44,14 @@ namespace ForceStep
             }
         }
 
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Switch to the main thread - the call to AddCommand in ManuallyEnable's constructor requires
-            // the UI thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
             OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new ManuallyEnable(package, commandService);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
